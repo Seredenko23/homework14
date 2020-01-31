3e2a05279f4fc7b36f505ae347ffd0c2f65d1e5b
Vlad <srvladyk555@gmail.com>
2020-01-31 11:44:42 +0200

    init project and create userList and user component

---
+++ src/app/user-list/user-list.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { UserComponent } from './user/user.component';
+
+@NgModule({
+  imports: [],
+  declarations: [UserComponent],
+})
+
+export class userListModule { }
